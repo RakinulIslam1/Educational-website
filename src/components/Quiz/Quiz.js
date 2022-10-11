ddd8b32dbb52ad32f9418cc4b124c8a67d2eb5d9
راكينول إسلام <you@example.com>
2022-10-11 10:17:31 +0600

    data loaded

---
+++ src/components/Quiz/Quiz.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Quiz.css';
+
+const Quiz = ({data}) => {
+    const { name, logo } = data;
+    return (
+      <div className="quiz">
+        <img src={logo} alt="" />
+        <div className='card-details'>
+          <h1 className="text-xl font-bold">{name}</h1>
+          <button className="bg-blue-700 text-white p-2 rounded-md font-semibold">
+            Start Quiz
+          </button>
+        </div>
+      </div>
+    );
+};
+
+export default Quiz;
--- src/components/Topics/Topics.js
+++ src/components/Topics/Topics.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
+import Quiz from '../Quiz/Quiz';
 
 const Topics = () => {
+    const loadData = useLoaderData();
+    const datas = loadData.data;
+    console.log(datas);
     return (
         <div>
-            <h2>Some topics</h2>
+            <h2>Some topics {datas.length}</h2>
+            {
+                datas.map(data => <Quiz 
+                key={data.id}
+                data={data}
+                ></Quiz>)
+            }
         </div>
     );
 };
